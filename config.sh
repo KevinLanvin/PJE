10e2a75f26a00f9ea7d664a7022bed0bacf4a3e4
Moyart Yann <moyart@b05p11.fil.univ-lille1.fr>
2015-10-21 16:37:23 +0200

    modif du config.sh

--- config.sh
+++ config.sh
@@ -2,3 +2,6 @@
 
 git config --global http.proxy "http://cache-etu.univ-lille1.fr:3128"
 git clone "http://github.com/Tikiwinkie/PJE.git"
+sudo ip link set eth1 up
+sudo ip addr add 192.168.1.2 dev eth1
+sudo ip route add 192.168.1.3 via 192.168.1.2 dev eth1
